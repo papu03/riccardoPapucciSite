0b25bb94b7ef22c3c6e63b947861ea3de25ebab5
unknown <riccardopapucci93@gmail.com>
2024-05-28 23:04:32 +0200

    add nginx in prod (css does not work)

---
+++ build-and-push-prod.cmd
@@ -0,0 +1,2 @@
+docker build -f ./Dockerfile -t papucci/riccardopapucci-prod:0.0.1 .
+docker push papucci/riccardopapucci-prod:0.0.1
---
+++ nginx-build-and-push-prod.cmd
@@ -0,0 +1,2 @@
+docker build -f ./_nginx/Dockerfile -t papucci/nginx-riccardopapucci:0.0.1 .
+docker push papucci/nginx-riccardopapucci:0.0.1
