b8b61935cb505aad46aaa04f725a4826f3e973d5
unknown <riccardopapucci93@gmail.com>
2024-10-30 23:49:00 +0100

    Fix MAIOR href

--- nginx-build-and-push-prod.cmd
+++ nginx-build-and-push-prod.cmd
@@ -1,2 +1,2 @@
-docker build -f Dockerfile -t papucci/nginx-riccardopapucci:1.0.2 .
-docker push papucci/nginx-riccardopapucci:1.0.2
+docker build -f Dockerfile -t papucci/nginx-riccardopapucci:1.0.4 .
+docker push papucci/nginx-riccardopapucci:1.0.4
