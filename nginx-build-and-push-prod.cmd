a884005d5d13ec862836388999e5252187098859
unknown <riccardopapucci93@gmail.com>
2024-11-05 23:10:08 +0100

    Update ivacon

--- nginx-build-and-push-prod.cmd
+++ nginx-build-and-push-prod.cmd
@@ -1,2 +1,2 @@
-docker build -f Dockerfile -t papucci/nginx-riccardopapucci:1.0.4 .
-docker push papucci/nginx-riccardopapucci:1.0.4
+docker build -f Dockerfile -t papucci/nginx-riccardopapucci:1.0.5 .
+docker push papucci/nginx-riccardopapucci:1.0.5
